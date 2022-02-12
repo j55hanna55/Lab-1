351e324c3f361cf1c70a15c98abf31d398f592c6
Jacob Hanna <jacobhanna@Jacobs-MacBook-Pro.local>
2022-02-11 16:58:10 -0800

    pushing my first code

---
+++ lab1.py
@@ -0,0 +1,20 @@
+#Ask user to enter the cents
+cent = int(input('How many cents ? '))
+qt="quarter"
+dm="dime"
+py="penny"
+rem1=cent%25
+quarter=int((cent-rem1)/25)
+rem2=rem1%10
+dime=int((rem1-rem2)/10)
+rem3=rem2%5
+penny=rem3
+if(quarter>1):
+   qt="quarters"
+if(dime>1): 
+   dm="dimes"
+if(penny>1):
+   py="pennies"
+   
+#display the output
+print(quarter,qt,",",dime,dm,penny,py)
